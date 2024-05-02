6e3f5542bc4097cf95bbacfbc41cc2353b79f592
otah11 <juanalbertoaraujo@gmail.com>
2024-05-02 00:46:56 -0300

    hw 3

---
+++ back/src/routes/appointmentRouter.ts
@@ -0,0 +1,6 @@
+// GET /appointment => Obtener todos los turnos
+// GET /appointment => obteer un turno por id
+
+// post / appointment/schedule => crear un nuevo turno
+
+// PUT / appointment/ cancel => cancelar un turno
---
+++ back/src/routes/usersRouter.ts
@@ -0,0 +1,4 @@
+// get/ user => obetener los usuarios
+// get / users / id: => obtener un usuario
+
+// POST /user/register => Crear un nuevo usuario
