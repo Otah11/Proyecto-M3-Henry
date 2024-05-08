7a95418a0f872596919ee54889a8f214040b1130
otah11 <juanalbertoaraujo@gmail.com>
2024-05-07 21:41:21 -0300

    la wea .env

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 dotenv.config({path:"./src/config/.env"});
 
 
-const{PORT,PROTO,HOST}=process.env
+export const PORT = process.env.PORT || 3000;
+export const PROTO = process.env.PROTO || "http";
+export const HOST = process.env.HOST || "localhost";
 
-
-export{
-    PORT,
-    PROTO,
-    HOST
-}
+export const DB_PORT = Number(process.env.DB_PORT) || 5432;
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USERNAME = process.env.DB_USERNAME || "postgres";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "test";
+export const DB_NAME = process.env.DB_NAME || "test";
